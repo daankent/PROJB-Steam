782abdaba99aaabc6e8764db07ee32fd4e4a1ef2
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-11-21 18:31:08 +0100

    sortedGames functie toegevoegd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 
 def loadJsonData():
-    """Functie die de data uit het json bestand inlaadt en returned.)"""
+    """Functie die de data uit het json bestand inlaadt en returned."""
 
     # bestand openen
     file = open('steam.json')
@@ -15,3 +15,17 @@ def loadJsonData():
 
     # data returnen
     return data
+
+
+# TODO: Eigen sorteerfuncties implementeren (alfabetisch, release datum, ratings, speeltijd, prijs, owners, required age)
+# TODO: Bepaalde symbolen negeren tijdens het sorteren
+def sortedGames(reversed=False):
+    """Functie die de ingeladen games uit het json bestand sorteert op alfabetische volgorde van de naam. \n Als reverse == True dan worden de games teruggegeven in omgekeerd alfabetische volgorde."""
+    # games inladen met de loadJsonData functie
+    games = loadJsonData()
+
+    # games sorteren op alfabetische volgorde (spaties, " en ' weghalen aan begin en eind
+    games.sort(key=lambda game: game['name'].strip(' " \''), reverse=reversed)
+
+    # gesorteerde games returnen
+    return games
