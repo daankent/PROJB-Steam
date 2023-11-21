a76c4a3669cd93d80302d3a082f5ff82725d39f4
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-11-21 18:10:33 +0100

    loadJsonData functie toegevoegd

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import json
+
+
+def loadJsonData():
+    """Functie die de data uit het json bestand inlaadt en returned.)"""
+
+    # bestand openen
+    file = open('steam.json')
+
+    # json inladen
+    data = json.load(file)
+
+    # bestand sluiten
+    file.close()
+
+    # data returnen
+    return data
