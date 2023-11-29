e775a6890c1ea38ecfedb11a225d0e9246bd0835
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-11-29 15:45:50 +0100

    playerInfo route toegevoegd

--- api/main.py
+++ api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import getPlayerInfo
+import json
 
 app = FastAPI()
 
 
 @app.get("/")
 def get_slash():
-    return "Hello world!"
+    return "Hello world"
+
+
+@app.get("/playerInfo/")
+def playerInfo(ids):
+    data = getPlayerInfo.getPlayerInfo(json.loads(ids))
+    return data
