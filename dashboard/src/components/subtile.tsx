6f89eabf929ce3e5617ad4284b49e6171a3a95cf
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-12-06 14:40:46 +0100

    subtile toegevoegd

---
+++ dashboard/src/components/subtile.tsx
@@ -0,0 +1,8 @@
+// Tegel component
+export default function SubTile({ children }: any) {
+  return (
+    <div className=" bg-[#D9D9D9] bg-opacity-50 p-4 rounded-md mb-4 shadow-md">
+      {children}
+    </div>
+  );
+}
