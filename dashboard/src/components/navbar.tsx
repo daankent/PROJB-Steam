ac6526ffc3bc3a26368ac978734f97a0869b9b7e
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-12-07 11:20:19 +0100

    NavbarMenu toegevoegd

--- dashboard/src/components/navbar.tsx
+++ dashboard/src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { cookies } from "next/headers";
-
+import NavbarMenu from "./navbarMenu";
 export default function Navbar() {
   const id = cookies().get("steamid");
   let user = undefined;
@@ -11,29 +11,34 @@ export default function Navbar() {
   }
 
   return (
-    <div className="flex flex-row bg-donkerblauw border-b-2 border-b-white">
-      <div className="flex items-center p-4 flex-1 ">
-        <Link href="/">
-          <h1 className="text-lichtgrijs font-bold ">
-            STEAM<span className="text-blauw">HUB</span>
-          </h1>
-        </Link>
-      </div>
-      {id && user && (
-        <div className="flex flex-row items-center mr-4">
-          <Link href="/uitloggen">
-            <h1 className="text-lichtgrijs mr-4 font-bold">
-              {user["personaname"]}
+    <div className="flex flex-col">
+      <div className="flex flex-row bg-donkerblauw border-b-2 border-b-white z-0">
+        <div className="flex items-center p-4 flex-1 ">
+          <Link href="/">
+            <h1 className="text-lichtgrijs font-bold ">
+              STEAM<span className="text-blauw">HUB</span>
             </h1>
           </Link>
-
-          <img
-            className="rounded-full"
-            src={user["avatar"]}
-            alt="user avatar"
-          />
         </div>
-      )}
+        {id && user && (
+          <>
+            {/* <div className="flex flex-row items-center mr-4">
+              <Link href="/uitloggen">
+                <h1 className="text-lichtgrijs mr-4 font-bold">
+                  {user["personaname"]}
+                </h1>
+              </Link>
+
+              <img
+                className="rounded-full"
+                src={user["avatar"]}
+                alt="user avatar"
+              />
+            </div> */}
+            <NavbarMenu user={user} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ dashboard/src/components/navbarMenu.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function NavbarMenu({ user }: any) {
+  const router = useRouter();
+  const [open, setOpen] = useState<boolean>(false);
+  function toggle() {
+    setOpen(!open);
+  }
+  useEffect(() => {
+    router.refresh();
+  }, []);
+  return (
+    <>
+      <div className="flex flex-row items-center mr-4" onClick={toggle}>
+        <h1 className="text-lichtgrijs mr-4 font-bold cursor-pointer">
+          {user.personaname}
+        </h1>
+
+        <img
+          className="rounded-full cursor-pointer"
+          src={user.avatar}
+          alt="user avatar"
+        />
+      </div>
+      {open && (
+        <div className=" w-full flex flex-row-reverse z-10 p-4 rounded-md absolute mt-8">
+          <div className="bg-lichtgrijs w-full md:w-[25%] lg:w-[15%]  rounded-md p-4 flex flex-row-reverse">
+            <div className="flex flex-col">
+              <Link href="/uitloggen">
+                <p className="text-red-900 font-bold cursor-pointer">
+                  Uitloggen
+                </p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
