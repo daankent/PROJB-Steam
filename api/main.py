4c0630559f609e71da1d63a51f941463298fc0d2
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2023-12-17 14:26:27 +0100

    cors en cache toegevoegd aan api

--- api/main.py
+++ api/main.py
@@ -3,9 +3,24 @@ import getPlayerInfo
 import getPlayerOwnedGames
 import getPlayerFriendList
 import getPlayerLastPlayed
-
+import sortOwnedGames
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = [
 
+  "http://localhost:3000"
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+cache = {}
 
 @app.get("/")
 def get_slash():
@@ -18,10 +33,31 @@ def playerInfo(ids):
     return data
 
 
-@app.get("/playerOwnedGames/")
-def playerOwnedGames(id):
-    data = getPlayerOwnedGames.getPlayerOwnedGames(id)
-    return data
+@app.get("/playerOwnedGames/{sort}")
+def playerOwnedGames(id, sort):
+    cache_key = f'{sort}-{id}'
+    print(cache_key)
+    if cache_key in cache:
+        print("CACHEEEEEE")
+        return cache[cache_key]
+    else:
+        data = getPlayerOwnedGames.getPlayerOwnedGames(id)
+        
+    if sort == "az":
+        cache[cache_key] = sortOwnedGames.asc(data["response"]["games"], "name")
+        return cache[cache_key] 
+    elif sort == "za":
+        cache[cache_key] = sortOwnedGames.desc(data["response"]["games"], "name")
+        return cache[cache_key] 
+    elif sort == "playtime-asc":
+        cache[cache_key] = sortOwnedGames.asc(data["response"]["games"], "playtime_forever")
+        return cache[cache_key] 
+    elif sort =="playtime-desc":
+        cache[cache_key] = sortOwnedGames.desc(data["response"]["games"], "playtime_forever")
+        return cache[cache_key] 
+    else:
+        cache[cache_key] = data["response"]["games"]
+        return data["response"]["games"]
 
 
 @app.get("/playerFriends/")
