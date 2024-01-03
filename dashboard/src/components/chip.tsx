bdc01856baaf471d625e72dd85be8361a60c71e0
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2024-01-03 19:22:10 +0100

    chip component toegevoegd

---
+++ dashboard/src/components/chip.tsx
@@ -0,0 +1,8 @@
+// chip component
+export default function Chip({ children }: any) {
+  return (
+    <div className=" bg-[#D9D9D9] bg-opacity-50 mx-2 my-2 rounded-full  shadow-md">
+      {children}
+    </div>
+  );
+}
