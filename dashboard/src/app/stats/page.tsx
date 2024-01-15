30a2c0d842d4c03a5d064a3785b096f44f9ce608
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2024-01-15 13:05:22 +0100

    SpeeltijdList toegevoegd aan dashboard

---
+++ dashboard/src/app/stats/page.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import { cookies } from "next/headers";
+import Navbar from "@/components/navbar";
+import Section from "@/components/section";
+import Tile from "@/components/tile";
+import { FaChevronRight } from "react-icons/fa6";
+import LoggedFrame from "@/components/loggedIn";
+import { API_URL } from "@/APIURL";
+import GenreStats from "@/components/stats/genreStats";
+import PrijsSpeeltijdStats from "@/components/stats/prijsSpeeltijd";
+
+async function getGenreData() {
+  const steamId = cookies().get("steamid")?.value;
+  const res = await fetch(`${API_URL}/genrestats/`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    // throw new Error("Fout bij het ophalen van de games");
+    return [];
+  }
+  return res.json();
+}
+
+async function getPrijsSPeeltijdData() {
+  const steamId = cookies().get("steamid")?.value;
+  const res = await fetch(`${API_URL}/priceplaytimestats/`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    // throw new Error("Fout bij het ophalen van de games");
+    return [];
+  }
+  return res.json();
+}
+export default async function StatsPage() {
+  const genreData = await getGenreData();
+  const genres = genreData;
+
+  const prijsSpeeltijdData = await getPrijsSPeeltijdData();
+  const prijsSpeeltijd = prijsSpeeltijdData;
+  return (
+    <div>
+      <LoggedFrame>
+        <Navbar />
+        <Section>
+          <h1 className="text-lichtgrijs  font-bold text-2xl italic mb-2">
+            Stats
+          </h1>
+          <div className="w-full grid grid-cols-10 gap-10">
+            <div className="col-span-5">
+              <Tile>
+                <h2 className="text-lichtgrijs  font-bold text-xl italic mb-2">
+                  Game genres:
+                </h2>
+                <p className="italic text-lichtgrijs  mb-2">
+                  Visualistatie van alle het aantal keer dat een genre voorkomt
+                  in alle steam games.
+                </p>
+                <GenreStats data={genres} />
+              </Tile>
+            </div>
+            <div className="col-span-5">
+              <Tile>
+                <h2 className="text-lichtgrijs  font-bold text-xl italic mb-2">
+                  Prijs vs Speeltijd:
+                </h2>
+                {/* <p className="italic text-lichtgrijs  mb-2">
+                  Visualistatie van alle het aantal keer dat een genre voorkomt
+                  in alle steam games.
+                </p> */}
+                <PrijsSpeeltijdStats data={prijsSpeeltijd} />
+              </Tile>
+            </div>
+          </div>
+        </Section>
+      </LoggedFrame>
+    </div>
+  );
+}
---
+++ dashboard/src/components/home/speelmomenten.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import { cookies } from "next/headers";
+import SubTile from "@/components/subtile";
+import { API_URL } from "@/APIURL";
+import { FaChevronRight } from "react-icons/fa6";
+async function getData() {
+  const steamId = cookies().get("steamid")?.value;
+  const res = await fetch(`${API_URL}/speelmomenten?id=${steamId}`, {
+    next: { revalidate: 600 },
+  });
+  if (!res.ok) {
+    throw new Error("Fout bij het ophalen van de vrienden");
+  }
+  return res.json();
+}
+
+export default async function SpeelmomentenList() {
+  const data = await getData();
+  const speelmomenten = data;
+  let i = 0;
+  return (
+    <>
+      <div className="flex flex-col">
+        {speelmomenten.private?.deelnames?.map((m: any) => {
+          return (
+            <SubTile key={m.sid}>
+              <div className="flex flex-row items-center">
+                <div className="flex-1">
+                  <h1 className="text-donkerblauw font-bold">
+                    {new Date(m.datum).toDateString()}{" "}
+                    {m.starttijd.split(":")[0]}:{m.starttijd.split(":")[1]}-
+                    {m.eindtijd.split(":")[0]}:{m.eindtijd.split(":")[1]}
+                  </h1>
+                  <h2>{m.game_name}</h2>
+                </div>
+                <Link href={`/speelmoment/info/${m.sid}`}>
+                  <div className="bg-blauwgrijs p-2 rounded-md text-lichtgrijs hover:bg-donkerblauw">
+                    <FaChevronRight />
+                  </div>
+                </Link>
+              </div>
+            </SubTile>
+          );
+        })}
+        <h1 className="text-donkerzwartblauw font-bold text-xl italic mb-2">
+          Uitnodigingen
+        </h1>
+        {speelmomenten.private.uitnodigingen.length <= 0 && (
+          <h3 className="text-lichtgrijs  font-bold text-lg italic mb-2">
+            Je hebt momenteel geen uitnodigengen
+          </h3>
+        )}
+        {speelmomenten.private.uitnodigingen.map((m: any) => {
+          return (
+            <SubTile key={m.sid}>
+              <div className="flex flex-row items-center">
+                <div className="flex-1">
+                  <h1 className="text-donkerblauw font-bold">
+                    {new Date(m.datum).toDateString()}{" "}
+                    {m.starttijd.split(":")[0]}:{m.starttijd.split(":")[1]}-
+                    {m.eindtijd.split(":")[0]}:{m.eindtijd.split(":")[1]}
+                  </h1>
+                  <h2>{m.game_name}</h2>
+                </div>
+
+                <Link href={`/speelmoment/info/${m.sid}`}>
+                  <div className="bg-blauwgrijs p-2 rounded-md text-lichtgrijs hover:bg-donkerblauw">
+                    <FaChevronRight />
+                  </div>
+                </Link>
+              </div>
+            </SubTile>
+          );
+        })}
+      </div>
+
+      <div className="flex flex-row justify-end col-span-3 italic"></div>
+    </>
+  );
+}
