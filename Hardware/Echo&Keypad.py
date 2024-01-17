f418bddb0b1f042f5caf6f9845c9481a888da96e
SuitedPuinguin <kevinhabes@gmail.com>
2024-01-17 05:09:14 +0100

    Echo&Keypad code voor pc en pico netjes opgemaakt

---
+++ Hardware/Echo&Keypad.py
@@ -0,0 +1,33 @@
+from serial.tools import list_ports
+import serial
+
+
+def read_serial(port):
+    """Read data from serial port and return as string."""
+    line = port.read(1000)
+    return line.decode()
+
+
+# First manually select the serial port that connects to the Pico
+serial_ports = list_ports.comports()
+
+print("[INFO] Serial ports found:")
+for i, port in enumerate(serial_ports):
+    print(str(i) + ". " + str(port.device))
+
+pico_port_index = int(input("Which port is the Raspberry Pi Pico connected to? "))
+pico_port = serial_ports[pico_port_index].device
+
+# Open a connection to the Pico
+with serial.Serial(port=pico_port, baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=1) as serial_port:
+    if serial_port.isOpen():
+        print("[INFO] Using serial port", serial_port.name)
+    else:
+        print("[INFO] Opening serial port", serial_port.name, "...")
+        serial_port.open()
+
+
+    while True:
+        serial_port.flush()
+        mes = serial_port.read_until().strip()
+        print(mes.decode())
