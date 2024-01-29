91b0f711cfbae1cf77a2c3e35ee7333e3d74ad39
Daan Kentrop <43522330+daankent@users.noreply.github.com>
2024-01-29 12:45:52 +0100

    bugfix developerstats

--- dashboard/src/components/stats/developerStats.tsx
+++ dashboard/src/components/stats/developerStats.tsx
@@ -69,7 +69,10 @@ export default function DeveloperStats({}: any) {
                     stats.kwal &&
                     stats.kwal.map((s: any) => {
                       return (
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <tr
+                          key={s.genre}
+                          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                        >
                           <th
                             scope="row"
                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
